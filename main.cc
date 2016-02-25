20990aee58d65ed51d1f93df46f535d82f79c91d
Remy Sun <Remy.Sun@ens-rennes.fr>
2016-02-25 23:29:45 +0100

    alright I have some data, on to treating it

--- main.cc
+++ main.cc
@@ -1,7 +1,29 @@
 #include "main.hh"
 #include <time.h>
+#include <cmath>
 
 using namespace std;
+
+double mean(vector<double> v)
+{
+  double sum = 0.0;
+  unsigned i;
+  for (i=0;i<v.size();i++)
+    sum += v.at(i);
+  return sum / (double)v.size();
+}
+
+double stdvar(vector<double> v)
+{
+  double sum = 0.0;
+  unsigned i;
+  for (i=0;i< v.size();i++)
+    {
+      double avg= mean(v);
+      sum += (v.at(i) - avg)*(v.at(i) - avg);
+    }
+  return sqrt(sum / (double)v.size());
+}
 
 int main() 
 {    
@@ -20,10 +42,10 @@ int main()
     
     ofstream log("log2.csv");
     
-    cout << "n;inverse;inverse2;test(s)" << endl;
-    log << "n;inverse;inverse2;test(s)" << endl;
+    cout << "n;inverse;stdvar;inverse2;stdvar2;test(s)" << endl;
+    log << "n;inverse;stdvar;inverse2;stdvar2;test(s)" << endl;
     
-    for(n = 2; n < 10; n++)
+    for(n = 2; n < 8; n++)
     {
         cout << n << ";" << flush;
         max /= n;
@@ -31,33 +53,47 @@ int main()
             max = 1;
         begin = clock();
         
+	vector<double> slow = vector<double>(max,0.0);
+	
         for(i = 0; i < max; i++)
         {
             Matrix M = Matrix::random(n, n, -10.0, 10.);
             Matrix M2(M.inverse());
+	    end = clock();
+	    slow.at(i) = (double)(end - begin) / CLOCKS_PER_SEC;
+	    begin = end;
+	    
         }
         
-        end = clock();
         
-        cout << setw(8) << (double)(end - begin) / CLOCKS_PER_SEC / max << ";" << flush;
-        log << setw(8) << (double)(end - begin) / CLOCKS_PER_SEC / max << ";" << flush;
-        
+        cout << setw(8) << mean(slow) << ";" << stdvar(slow) << ";" << flush;
+        log << setw(8) << mean(slow) << ";"  << stdvar(slow) << ";" << flush;
+
+	vector<double> fast = vector<double>(max,0.0);        
+
         begin = clock();
         
+
         for(i = 0; i < max; i++)
         {
             Matrix M = Matrix::random(n, n, -10.0, 10.);
             M.use_static(true);
             Matrix M2(M.inverse());
+	    end = clock();
+	    fast.at(i) = (double)(end - begin) / CLOCKS_PER_SEC;
+	    begin = end;
         }
         
-        end = clock();
         
-        cout << setw(8) << (double)(end - begin) / CLOCKS_PER_SEC / max << ";" << max << endl;
-        log << setw(8) << (double)(end - begin) / CLOCKS_PER_SEC / max << ";" << max << endl;
+        cout << setw(8) << mean(fast) << ";" << stdvar(fast) << ";" << max << endl;
+        log << setw(8) << mean(fast) << ";"  << stdvar(fast) << ";" << max << endl;
     }
     
     
+
+
+
+
     
     return 0;
 }
